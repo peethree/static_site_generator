f98d14203d0fce7ca05f26f1605708941c7528d9
peethree <pieterschoptkont@gmail.com>
2024-08-12 12:12:54 +0200

    added a function to main to copy files from static into a public directory

--- src/main.py
+++ src/main.py
@@ -4,14 +4,50 @@ from text_node_to_html_node import text_node_to_html_node
 from split_nodes_delimiter import split_nodes_delimiter
 from extract_markdown import extract_markdown_images, extract_markdown_links
 from split_nodes import split_nodes_image, split_nodes_link
+from markdown_to_html_node import markdown_to_html_node
+import os
 import re
+import shutil
 
 
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    result = text_node_to_html_node(node)
-    print(result)
+    # remove public if it exists
+    if os.path.exists("public"):
+        shutil.rmtree("public")      
 
+    if os.path.exists("static"):
+        # get the files in static
+        list_of_paths = dig_for_files("static")       
+
+    # copy over static files to public
+    for path in list_of_paths:     
+        relative_path = os.path.relpath(path, "static")               
+        destination_path = os.path.join("public", relative_path)
+
+        # make directories if they don't exist (public, public/images/)
+        os.makedirs(os.path.dirname(destination_path))                
+        shutil.copy(path, destination_path)
+
+
+def dig_for_files(dir):    
+
+    file_paths = []
+
+    for item in os.listdir(dir):
+        # ['index.css, images]
+        item_path = os.path.join(dir, item)
+        # static/index.css, static/images/
+
+        # base case: current path leads to a file
+        if os.path.isfile(item_path):
+            file_paths.append(item_path)
+
+        # if it's directory, recursively call dig_for_files to reach the file
+        if os.path.isdir(item_path):
+            # [static/images]
+            file_paths.extend(dig_for_files(item_path))  
+
+    return file_paths     
 
 
 if __name__ == "__main__":
